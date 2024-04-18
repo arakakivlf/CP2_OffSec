fadf9d4a2c72a1a9d9c3046f9899878803dab245
arakakivlf <160850726+arakakivlf@users.noreply.github.com>
2024-04-18 02:39:39 -0300

    Sniffing async

--- main.py
+++ main.py
@@ -25,19 +25,26 @@ try:
     print("\nUnanswered ones:")
     unans.summary(lambda s: s.sprintf(f"%TCP.dport%\t\tFiltered"))
 
-    # print("Sending ICMP echo-request instruction to the vulnerable machine...")
-    # Exploiting any available service...
+    # print("Exploiting the vulnerable service.")
+    # Exploiting the vulnerable service.
     # Running some instructions... injecting commands...
+    
+    # Asynchronously Sniffing network activity
+    cap = AsyncSniffer(iface = "eth0", count = 5, filter = "icmp and host 99.99.99.254", prn = lambda p: p.sprintf("Received ICMP packet from %IP.dst%"))
+    cap.start()
+
+    # print("Sending ICMP echo-request instruction to the vulnerable machine...")
     # Sending ICMP echo-request command as instruction to the vulnerable machine to execute.
     # Something like the following packet could be constructed and sended:
     # inj_packet = IP(dst = DEST_IP_ADDR)/TCP(dport = VULNERABLE_SERVICE_PORT)/"| ping 99.99.99.50 -n 5"
     # sr(inj_packet)
+    
+    time.sleep(10)
+    if cap.running: cap.stop()
 
-    # Sniffing network activity
-    cap = sniff(iface = "eth0", count = 5, filter = "icmp and host 99.99.99.254", prn = lambda p: p.sprintf("Received ICMP packet from %IP.dst%"))
-
-    # Saving captured data in a pcap file.
-    wrpcap("captured.pcap", cap)
+    # Writting to pcap file
+    if len(cap.results) > 0:
+        wrpcap("capture.pcap", cap.results)
 except Exception as e:
     print("An exception has occurred:")
     print(e)
